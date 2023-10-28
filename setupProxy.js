c35d49993712e98323f7aa02d07f866c58568a74
Eunji Yoo <enjiu9932@naver.com>
2023-10-28 15:22:19 +0900

    proxy 변경

--- setupProxy.js
+++ setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware('/api', '/PlantsPlanet', '/', {
-      target: 'http://ec2-3-34-199-199.ap-northeast-2.compute.amazonaws.com:8083',
+      target: 'http://ec2-52-78-9-20.ap-northeast-2.compute.amazonaws.com:8083/',
       changeOrigin: true,
     })
   );
